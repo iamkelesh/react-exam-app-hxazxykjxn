ecdec3b1e7bc3701c0dfa5d8d0e35760d705a47e
Deyan Kamburov <71717701+iamkelesh@users.noreply.github.com>
2024-08-08 01:27:54 +0300

    updated navbar

--- client-app/src/components/NavBarComp.jsx
+++ client-app/src/components/NavBarComp.jsx
@@ -2,31 +2,32 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom'; 
 
 function NavBarComp() {
     return (
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Home</Navbar.Brand>
+                <Navbar.Brand as={Link} to={'/home'}>Home</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#all-topics">All topics</Nav.Link>
-                        <Nav.Link href="#create-discussion">Create discusion</Nav.Link>
-                        <Nav.Link href="#login">Login</Nav.Link>
-                        <Nav.Link href="#register">Register</Nav.Link>
-                        <Nav.Link href="#account">Account</Nav.Link>
-                        <NavDropdown title="Theme" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="#topics/games">Games</NavDropdown.Item>
+                        <Nav.Link as={Link} to={'/all-topics'}>All topics</Nav.Link>
+                        <Nav.Link as={Link} to={'/create-discusion'}>Create discusion</Nav.Link>
+                        <Nav.Link as={Link} to={'login'}>Login</Nav.Link>
+                        <Nav.Link as={Link} to={'/register'}>Register</Nav.Link>
+                        <Nav.Link as={Link} to={'/account'}>Account</Nav.Link>
+                        <NavDropdown title="Topics" id="collapsible-nav-dropdown">
+                            <NavDropdown.Item as={Link} to={'/topics/games'}>Games</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#topics/movies">Movies</NavDropdown.Item>
-                            <NavDropdown.Item href="#topics/anime">Anime</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to={'/topics/movies'}>Movies</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to={'/topics/anime'}>Anime</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#topics/books">
+                            <NavDropdown.Item as={Link} to={'/topics/books'}>
                                 Books
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#topics/manga">Manga</NavDropdown.Item>
-                            <NavDropdown.Item href="#topics/comics">
+                            <NavDropdown.Item as={Link} to={'/topics/manga'}>Manga</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to={'/topics/comics'}>
                                 Comics
                             </NavDropdown.Item>
                         </NavDropdown>
