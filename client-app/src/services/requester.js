2a694a8e2ace884a8d5423bae898eda2e395c7ee
Deyan Kamburov <71717701+iamkelesh@users.noreply.github.com>
2024-08-08 05:32:46 +0300

    created requester service

---
+++ client-app/src/services/requester.js
@@ -0,0 +1,32 @@
+const requester = async (method, url, data) => {
+    let options = {}
+
+    if (data) {
+        options.body = JSON.stringify(data)
+        options.headers = {
+            'Content-Type': 'application/json'
+        }
+
+    }
+
+    let response = await fetch(url, { method, ...options })
+
+    if (response.status === 204) {
+        return {};
+    }
+
+    let result = await response.json()
+
+    if (!response.ok) {
+        throw result;
+    }
+
+    return result
+}
+
+
+
+export const get = requester.bind(null, 'GET')
+export const post = requester.bind(null, 'POST')
+export const put = requester.bind(null, 'PUT')
+export const remove = requester.bind(null, 'DELETE')
