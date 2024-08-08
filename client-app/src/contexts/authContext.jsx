d6715748320a79436df9b669924c5965403d6294
Deyan Kamburov <71717701+iamkelesh@users.noreply.github.com>
2024-08-08 06:29:24 +0300

    created authCOntext

---
+++ client-app/src/contexts/authContext.jsx
@@ -0,0 +1,34 @@
+import { createContext } from "react"
+import { useNavigate } from "react-router"
+import { register } from "../services/authServices"
+
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+
+    const navigate = useNavigate()
+    const [authState, setAuthState] = useState({})
+
+    const registerSubmitHandler = async (name, email, password) => {
+        const result = await register(name, email, password)
+
+        setAuthState(result)
+
+        navigate('/')
+    }
+
+    const values = {
+        registerSubmitHandler,
+        isAuthenticated: Boolean(authState.accessToken),
+        username: authState.username,
+        email: authState.email,
+        userId: authState._id,
+
+    }
+
+    return (
+        <AuthContext.Provider value={values}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
